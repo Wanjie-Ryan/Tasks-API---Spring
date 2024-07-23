e80b08b7949a76cf1a627517c83d541326546ce7
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-07-23 18:22:56 +0300

    implementing the authentication service

--- src/main/java/com/tasks/api/projects/service/AuthService.java
+++ src/main/java/com/tasks/api/projects/service/AuthService.java
@@ -3,6 +3,7 @@ package com.tasks.api.projects.service;
 import com.tasks.api.projects.configs.JwtService;
 import com.tasks.api.projects.models.Auth;
 import com.tasks.api.projects.repository.AuthRepository;
+import com.tasks.api.projects.service.DTO.RegisterResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -21,7 +22,7 @@ public class AuthService {
 
     private AuthenticationManager authManager;
 
-    public Auth register(Auth auth){
+    public RegisterResponseDTO register(Auth auth){
 
         Auth registerAuth = new Auth();
         registerAuth.setName(auth.getName());
@@ -29,7 +30,14 @@ public class AuthService {
         registerAuth.setPassword(passwordEncoder.encode(auth.getPassword()));
         registerAuth.setRole(auth.getRole());
 
-        return authRepo.save(registerAuth);
+        Auth savedAuth = authRepo.save(registerAuth);
+        RegisterResponseDTO responseDto = new RegisterResponseDTO();
+        responseDto.setName(savedAuth.getName());
+        responseDto.setEmail(savedAuth.getEmail());
+        responseDto.setRole(savedAuth.getRole().toString());
+
+        return responseDto;
+
 
     }
 
