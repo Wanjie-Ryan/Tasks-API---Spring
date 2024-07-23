257f234861f119ab94c1026ad3e5643f3fbc04d6
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-07-23 16:45:27 +0300

    moving to security and jwt

--- src/main/java/com/tasks/api/projects/configs/JwtAuthFilter.java
+++ src/main/java/com/tasks/api/projects/configs/JwtAuthFilter.java
@@ -8,7 +8,12 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -37,6 +42,20 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
         jwt = authHeader.substring(7);
         userId = jwtService.extractUserId(jwt);
+
+        if(userId != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userId);
+            if(jwtService.isTokenvalid(jwt, userDetails)){
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+
+
+            }
+        }
+        filterChain.doFilter(request,response);
 
     }
 }
