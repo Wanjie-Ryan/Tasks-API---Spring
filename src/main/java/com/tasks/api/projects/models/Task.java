4c17311d44c616400beb443156b72fc3874d1a74
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-07-23 11:59:38 +0300

    the tasks model-done

--- src/main/java/com/tasks/api/projects/models/Task.java
+++ src/main/java/com/tasks/api/projects/models/Task.java
@@ -1,12 +1,13 @@
 package com.tasks.api.projects.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
@@ -18,8 +19,34 @@ public class Task {
 
     @Id
     private UUID id;
+
+    @NotBlank(message = "Task Name is Required")
     private String name;
+
+    @NotBlank(message = "Task Description is Required")
     private String description;
+
+    @NotNull(message = "Task completion status is Required")
+    private Boolean completed = false;
+
+    @NotNull(message = "Task creation time is required")
+    private LocalDateTime createdAt;
+
+    @NotNull(message = "Task deadline time is required")
+    private LocalDateTime deadline;
+
+    @ManyToOne
+    @NotNull(message = "Admin is Required")
+    @JoinColumn(name="project_id", nullable = false)
+    private Project project;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+        if (this.id == null) {
+            this.id = UUID.randomUUID();
+        }
+    }
 
 
 }
