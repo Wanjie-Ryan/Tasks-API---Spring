b5ac3c5a3a93faf66c7d88f050cb0ed33ab70b3e
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-07-23 19:02:58 +0300

    implementing the authentication controller

--- src/main/java/com/tasks/api/projects/controller/AuthController.java
+++ src/main/java/com/tasks/api/projects/controller/AuthController.java
@@ -1,11 +1,32 @@
 package com.tasks.api.projects.controller;
 
+import com.tasks.api.projects.models.Auth;
+import com.tasks.api.projects.repository.AuthRepository;
+import com.tasks.api.projects.service.AuthService;
+import com.tasks.api.projects.service.DTO.RegisterResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @Autowired
+    private AuthRepository authRepo;
+
+    @PostMapping("/register")
+    public ResponseEntity<RegisterResponseDTO> register (@RequestBody Auth auth) throws Exception{
+
+
+
+    }
 
 
 
