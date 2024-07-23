5032d46fc8aecd3dad333d5ca3318031a5314d1b
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-07-23 18:15:47 +0300

    implementing the authentication service

--- src/main/java/com/tasks/api/projects/service/DTO/LoginDTO.java
+++
@@ -1,4 +0,0 @@
-package com.tasks.api.projects.service.DTO;
-
-public class LoginDTO {
-}
--- src/main/java/com/tasks/api/projects/service/DTO/RegisterResponseDTO.java
+++ src/main/java/com/tasks/api/projects/service/DTO/RegisterResponseDTO.java
@@ -1,9 +1,13 @@
 package com.tasks.api.projects.service.DTO;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-public class RegisterDTO {
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterResponseDTO {
 
     private String name;
     private String email;
