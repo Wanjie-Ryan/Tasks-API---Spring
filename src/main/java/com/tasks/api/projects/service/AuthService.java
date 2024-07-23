5dea8b4ac9f5c3f6c953101aa71ff9899a2f0934
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-07-23 17:57:48 +0300

    implementing the authentication service

--- src/main/java/com/tasks/api/projects/service/AuthService.java
+++ src/main/java/com/tasks/api/projects/service/AuthService.java
@@ -1,6 +1,7 @@
 package com.tasks.api.projects.service;
 
 import com.tasks.api.projects.configs.JwtService;
+import com.tasks.api.projects.models.Auth;
 import com.tasks.api.projects.repository.AuthRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -19,6 +20,18 @@ public class AuthService {
     private JwtService jwtService;
 
     private AuthenticationManager authManager;
+
+    public Auth register(Auth auth){
+
+        Auth registerAuth = new Auth();
+        registerAuth.setName(auth.getName());
+        registerAuth.setEmail(auth.getEmail());
+        registerAuth.setPassword(passwordEncoder.encode(auth.getPassword()));
+        registerAuth.setRole(auth.getRole());
+
+        return authRepo.save(registerAuth);
+
+    }
 
 
 }
